06d7f083fd5538e345e02c86d9c9f9a054020d62
SabidMahmud <sabidmahmud96@gmail.com>
2026-09-11 23:28:08 +0600

    chore: update theme switch, sway config, and add solarized-dark theme

---
+++ themes/solarized-dark/gnome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+OMAKUB_THEME_COLOR="teal"
+OMAKUB_THEME_BACKGROUND="solarized-dark/background.png"
+source $OMAKUB_PATH/themes/set-gnome-theme.sh
---
+++ themes/solarized-dark/vscode.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VSC_THEME="Solarized Dark"
+VSC_EXTENSION="ryanolsonx.solarized"
+source $OMAKUB_PATH/themes/set-vscode-theme.sh
